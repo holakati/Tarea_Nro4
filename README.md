c26e3dfae3da6b641070f532f4282f092e4e210e
holakati <katiuska.hormazabalc@utem.cl>
2019-01-12 11:04:41 -0300

    actualización

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Tarea_Nro4
 Ejercicio de for-loop
+#___________________________
+
+#proc.time : Determina cuánto tiempo real y el tiempo de CPU (en segundos) que ha tomado el proceso R actualmente en ejecución.
+
+#for: Este comando es usado cuando de antemano sabemos el número de iteraciones que necesitamos.
+
+#print: muestra en pantalla el resultado de algo.
+
+# paste: Concatenar vectores después de convertir a carácter. 
+#Su uso: paste (., sep = " ", collapse = NULL)
+# .. : Uno o más objetos R , para convertirlos en vectores de caracteres.
+#sep: una cadena de caracteres para separar los términos. 
+#colapso: una cadena de caracteres opcional para separar los resultados. 
+
+#c: Esta es una función genérica que combina sus argumentos.
--- hola_hola.R
+++ hola_hola.R
@@ -1,5 +1,5 @@
 
-##5- Generar 500 ruts validos 
+##5- Generar ruts validos 
 
 Ruts_Validados <- list ()
 t <- proc.time()
@@ -18,4 +18,39 @@ for ( i  in sample ( 0000000 : 9999999 ,5000 ,replace = F )) {
 proc.time () -  t
 
 ##6- Construya una función que cuente palabras
+
+
+
+#7 completar archivo readme
+
+#8 crear lista de clientes
+clientes <- list (list(1,"Kati",-50),list(2,"Mati",-100),list(3,"joseti",50),list(4,"nicoti",-30))
+clientes
+contarSaldoNegativo <- function(listaclientes){
+  QGenteSaldoNegativo <- 0
+  for (cliente in listaclientes){
+    if (cliente[3] < 0){
+      QGenteSaldoNegativo<- (QGenteSaldoNegativo+1)
+    }
+  }
+  return(QGenteSaldoNegativo)
+}
+
+contarSaldoNegativo(clientes)
+
+#9 Sin vocales
+Frase <- "Tengo Hambre"
+
+sinVocales <- function(oracion){
+  stringsinvocales <- ""
+  strspliteada <- strsplit(oracion, "")[[1]]  
+  for (letra in strspliteada){
+    if(letra != "a" && letra != "e" && letra != "i" && letra != "o" && letra != "u") {
+      stringsinvocales <- paste(stringsinvocales, letra, sep="")
+    }
+  }
+  return(stringsinvocales)
+}
+sinVocales(Frase)
+
 
